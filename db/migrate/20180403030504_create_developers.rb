23bd0d9e722c21a663c085332b31e00723788796
Vuong Ngo <vuongngo@Sparks-PC.local>
2018-04-03 10:25:41 +0700

    Run generate scraffold Developer

---
+++ db/migrate/20180403030504_create_developers.rb
@@ -0,0 +1,11 @@
+class CreateDevelopers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :developers do |t|
+      t.string :name
+      t.string :email, null: false
+
+      t.timestamps
+    end
+    add_index :developers, :email, unique: true
+  end
+end
