b43ddd749640177d1d3fc24e4c813b5603a12047
Vuong Ngo <vuongngo@Sparks-PC.local>
2018-04-04 13:58:58 +0700

    Add model relationship

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,2 +1,4 @@
 class Language < ApplicationRecord
+  has_many :developer_languages
+  has_many :developers, through: :developer_languages
 end
--- app/models/programming_language.rb
+++ app/models/programming_language.rb
@@ -1,2 +1,4 @@
 class ProgrammingLanguage < ApplicationRecord
+  has_many :developer_programming_languages
+  has_many :developers, through: :developer_programming_languages
 end
