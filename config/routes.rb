eff35940ec3b88708a3de1ad278cfc9c7ece9069
Vuong Ngo <vuongngo@Sparks-PC.local>
2018-04-04 17:36:45 +0700

    Add Home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/index'
+  get '/search', to: 'developers#search'
+
   resources :developers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
