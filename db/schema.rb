3490dc6e53771b63c46a4a1f5e7a28777706ee23
Vuong Ngo <vuongngo@Sparks-PC.local>
2018-04-04 10:47:37 +0700

    Add ProgrammingLanguage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404034307) do
+ActiveRecord::Schema.define(version: 20180404034528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20180404034307) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["code"], name: "index_languages_on_code", unique: true
+  end
+
+  create_table "programming_languages", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_programming_languages_on_name", unique: true
   end
 
 end
